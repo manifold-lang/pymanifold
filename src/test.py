20be6eaa7df58a24440f822b4902b12a57377497
jsreid13 <js2reid@uwaterloo.ca>
2018-05-24 00:34:19 -0400

    Updated test script function's parameters

--- src/test.py
+++ src/test.py
@@ -8,18 +8,43 @@ continuous_node = 'continuous'
 dispersed_node = 'dispersed'
 output_node = 'out'
 junction_node = 't_j'
+min_channel_length = 0.01
+min_channel_width = 0.001
+min_channel_height = 0.001
+
 # Continuous and output node should have same flow rate
 # syntax: sch.port(name, design, pressure, flow_rate, X_pos, Y_pos)
 sch.port(continuous_node, 'input', 2, 7, 0, 0)
 sch.port(dispersed_node, 'input', 2, 7, 1, 1)
-sch.port(output_node, 'output', 1013, 5, 2, 0)
-sch.node(junction_node, 2, 1, 0, kind='t-junction')
-# syntax: sch.channel(shape, min_length, width, height, input, output)
-sch.channel('rectangle', 0.5, 0.1, 0.1, continuous_node,
-            junction_node, phase='continuous')
-sch.channel('rectangle', 0.5, 0.1, 0.1, dispersed_node,
-            junction_node, phase='dispersed')
-sch.channel('rectangle', 0.5, 0.1, 0.1, junction_node,
-            output_node, phase='output')
+sch.port(output_node, 'output', 1013, 7, 2, 0)
 
-print(sch.solve())
+# syntax: sch.node(name, X_pos, Y_pos, kind='node')
+sch.node(junction_node, 1, 0, kind='t-junction')
+
+# syntax: sch.channel(shape, min_length, width, height, input, output)
+sch.channel('rectangle',
+            min_channel_length,
+            min_channel_width,
+            min_channel_height,
+            continuous_node,
+            junction_node,
+            phase='continuous'
+            )
+sch.channel('rectangle',
+            min_channel_length,
+            min_channel_width,
+            min_channel_height,
+            dispersed_node,
+            junction_node,
+            phase='dispersed'
+            )
+sch.channel('rectangle',
+            min_channel_length,
+            min_channel_width,
+            min_channel_height,
+            junction_node,
+            output_node,
+            phase='output'
+            )
+
+print(sch.solve(show=True))
