bab04cd453ddb5b8b5985758ef35e9b160229214
Shubham Verma <shubhamv.sv@gmail.com>
2018-06-19 15:36:49 -0400

    Added resistivity value for polyacrylamide gel
    Signed-off-by: Shubham Verma <shubhamv.sv@gmail.com>

--- src/Constants.py
+++ src/Constants.py
@@ -1,9 +1,9 @@
 
 class FluidProperties():
 
-	PROPERTIES = {"water":[999.87, 182000, 0.001],
-				 "mineraloil": [800, 100000000000,0.0003051],
-				 "polyacrylamide":[1100, 0,0.003]}
+	PROPERTIES = {"water":[999.87, 18200, 0.001],
+				 "mineraloil": [800, 10000000000,0.0003051],
+				 "polyacrylamide":[1100, 14.28,0.003]}
 
 	def getDesnity(self, fluid_name):
 		return self.PROPERTIES[fluid_name][0]
