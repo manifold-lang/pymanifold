573314c4d8f277eb2be2d9088454c137545660d4
Yifan Mo <mo.yifan13@gmail.com>
2018-06-27 15:15:41 -0400

    update test file

--- src/get_test.py
+++ src/get_test.py
@@ -2,7 +2,7 @@ import pymanifold as pymf
 
 sch = pymf.Schematic([0, 0 , 10, 10])
 
-sch.port('in', 'input', min_pressure=100, min_viscosity=0.00089, density=1)
+sch.port('in', 'input', min_pressure=100, fluid_name='water')
 sch.port('out', 'output')
 sch.channel('in', 'out', min_length=1, min_width=0.9)
 
