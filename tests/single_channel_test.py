ed75490ef2966eff2e7bd1f2904e75d46bd8a4d5
Josh Reid <jsreid13@gmail.com>
2018-11-13 17:33:06 -0500

    Removed showing full formula

--- tests/single_channel_test.py
+++ tests/single_channel_test.py
@@ -5,7 +5,7 @@ sch = pymf.Schematic(dim=[0, 0, 10, 10])
 sch.port('in', 'input', fluid_name='water')
 sch.port('out', 'output')
 sch.channel('in', 'out', min_length=1, min_width=0.9)
-model = sch.solve(show=True)
+model = sch.solve()
 print(model)
 # change the computer location below to set where you want the json data to go
 #  sch.to_json("Insert your file location here")
