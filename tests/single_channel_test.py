7795538d61129aa5daa0b79a8676f242ad44513b
jsreid13 <js2reid@uwaterloo.ca>
2019-07-12 00:00:49 -0400

    Added parameters to tests

--- tests/single_channel_test.py
+++ tests/single_channel_test.py
@@ -2,10 +2,10 @@ import src.pymanifold as pymf
 
 sch = pymf.Schematic(dim=[0, 0, 10, 10])
 
-sch.port('in', 'input', fluid_name='water')
-sch.port('out', 'output')
-sch.channel('in', 'out', min_length=1, min_width=0.9)
-model = sch.solve()
+sch.port('in', kind='input', x=0.02, y=0.01, min_pressure=50, fluid_name='water')
+sch.port('out', kind='output', x=0.03, y=0.02)
+sch.channel('in', 'out')
+model = sch.solve(show=True)
 print(model)
 # change the computer location below to set where you want the json data to go
 #  sch.to_json("Insert your file location here")
--- tests/t_junction_test.py
+++ tests/t_junction_test.py
@@ -1,6 +1,6 @@
 import src.pymanifold as pymf
 
-sch = pymf.Schematic([0, 0, 10, 10])
+sch = pymf.Schematic([0, 0, 1, 1])
 #       D
 #       |
 #   C---N---O
@@ -8,23 +8,20 @@ continuous_node = 'continuous'
 dispersed_node = 'dispersed'
 output_node = 'out'
 junction_node = 't_j'
-min_channel_length = 1
-min_channel_width = 1
-min_channel_height = 0.001
 
 # Continuous and output node should have same flow rate
 # syntax: sch.port(name, design[, pressure, flow_rate, density, X_pos, Y_pos])
-sch.port(continuous_node, 'input', min_pressure=1) #, fluid_name='mineraloil')
-sch.port(dispersed_node, 'input', min_pressure=1) #, fluid_name='water')
-sch.port(output_node, 'output')
+sch.port(continuous_node, kind='input', fluid_name='mineraloil')
+sch.port(dispersed_node, kind='input', fluid_name='water')
+sch.port(output_node, kind='output')
 
 # syntax: sch.node(name, X_pos, Y_pos, kind='node')
-sch.node(junction_node, 1, 0, kind='tjunc')
+sch.node(junction_node, kind='tjunc')
 
 # syntax: sch.channel(shape, min_length, width, height, input, output)
-sch.channel(junction_node, output_node, phase='output')
-sch.channel(continuous_node, junction_node, phase='continuous')
-sch.channel(dispersed_node, junction_node, phase='dispersed')
+sch.channel(junction_node, output_node, min_height=0.0002, min_width=0.00021, phase='output')
+sch.channel(continuous_node, junction_node, min_height=0.0002, min_width=0.00021, phase='continuous')
+sch.channel(dispersed_node, junction_node, min_height=0.0002, min_width=0.00021, phase='dispersed')
 
 #  sch.solve()
 model = sch.solve(show=True)
