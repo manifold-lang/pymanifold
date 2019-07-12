9bd67db5338a788788acf7e47ba29a6895a88635
jsreid13 <js2reid@uwaterloo.ca>
2019-07-12 00:06:55 -0400

    Added parameters to tests

--- tests/node_test.py
+++ tests/node_test.py
@@ -2,11 +2,11 @@ import src.pymanifold as pymf
 
 sch = pymf.Schematic(dim=[0, 0, 10, 10])
 
-sch.port('in', 'input', x=3, y=3, fluid_name='water')
-sch.port('out', 'output', x=1, y=1)
-sch.node('middle node', x=3, y=1)
-sch.channel('in', 'middle node', min_length=2, min_width=0.9)
-sch.channel('middle node', 'out', min_width=0.9)
+sch.port('in', 'input', x=0.03, y=0.03, fluid_name='water')
+sch.port('out', 'output', x=0.01, y=0.01)
+sch.node('middle node', x=0.03, y=0.01)
+sch.channel('in', 'middle node', min_length=0.02, min_width=0.009)
+sch.channel('middle node', 'out', min_width=0.009)
 model = sch.solve(show=True)
 print(model)
 
